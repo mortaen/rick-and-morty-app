ff060021df2f63e40af462561faf3b6a74a3adbb
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-19 20:55:50 +0200

    add onClick functionality to first image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { useState } from "react"
 import "./App.css"
+import Audio from "./components/Audio/Audio"
+import trippyImg from "./img/trippy-overlay.gif"
 
 function App({ data }) {
   const rickSanchez = data.find((el) => el.id === 1)
 
+  // fear-and-loathing-overlay
+  const [overlay, setOverlay] = useState("")
+
+  function handleImageClick(input) {
+    input !== overlay ? setOverlay(input) : setOverlay("")
+  }
+
   return (
     <div className="App">
       <main className="main">
-        <section className="fear-and-loathing-section">
+        <section
+          className="fear-and-loathing-section"
+          onClick={() => handleImageClick("fearOverlay")}
+        >
+          {overlay === "fearOverlay" ? (
+            <img className="fear-overlay" src={trippyImg} alt="trippy gif" />
+          ) : (
+            ""
+          )}
+        </section>
+        <section className="heads-section">
+          <Audio />
           <img
             src={rickSanchez.image}
             className="rick-image"
             alt="Rick Sanchez"
           />
         </section>
-        <section className="heads-section"></section>
         <section className="mib-section"></section>
       </main>
     </div>
